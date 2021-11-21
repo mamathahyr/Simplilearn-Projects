5556db9ffd74b98af309d0b5c9021f1d7fd44036
Mamatha <41531258+mamathahyr@users.noreply.github.com>
2021-11-21 13:30:40 +0530

    Update Solution.java

--- src/final01/Solution.java
+++ src/final01/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        System.out.println("Program for basic file operation \nAuthor is Alexandr Orlov \n");
+        System.out.println("Welcome to the program - Virtual Key for Your Repositories!!! \nDeveloper is Mamatha S \n");
 
         Model model=new Model(Paths.get("D:\\Test folder"));
         Controller controller=new Controller(model);
